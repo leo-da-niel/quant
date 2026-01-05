c94af4a0358e9dfb217f272dd2790aea52437021
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2026-01-04 19:32:39 -0600

    Update swaps.py

--- derivatives/swaps.py
+++ derivatives/swaps.py
@@ -23,3 +23,23 @@ class InterestRateSwap:
             self.notional, bumped_rate, self.times, self.curve
         )
         return bumped_swap.pv() - self.pv()
+def parallel_shift(curve, shift):
+    shifted_dfs = [
+        df * np.exp(-shift * t)
+        for t, df in zip(curve.times, np.exp(curve.log_dfs))
+    ]
+    return ZeroCurve(curve.times, shifted_dfs)
+def bucketed_dv01(curve, swap, bump=1e-4):
+    dv01 = {}
+
+    for i, t in enumerate(curve.times):
+        bumped_dfs = np.exp(curve.log_dfs).copy()
+        bumped_dfs[i] *= np.exp(-bump * t)
+
+        bumped_curve = ZeroCurve(curve.times, bumped_dfs)
+        bumped_swap = InterestRateSwap(
+            swap.notional, swap.fixed_rate, swap.times, bumped_curve
+        )
+        dv01[t] = bumped_swap.pv() - swap.pv()
+
+    return dv01
