492a8de0f9f19a6eee35bc71a8818ca4c5a895ef
leo-da-niel <leo.da.niel.lechuga@gmail.com>
2026-01-04 19:23:31 -0600

    Update curves.py

--- core/curves.py
+++ core/curves.py
@@ -14,3 +14,17 @@ def build_zero_curve(times, rates):
     rates : array-like (zero rates)
     """
     return dict(zip(times, rates))
+
+
+class ZeroCurve:
+
+    def __init__(self, times, dfs):
+        self.times = np.array(times)
+        self.log_dfs = np.log(np.array(dfs))
+
+    def discount(self, t):
+        log_df = np.interp(t, self.times, self.log_dfs)
+        return np.exp(log_df)
+
+    def zero_rate(self, t):
+        return -np.log(self.discount(t)) / t
